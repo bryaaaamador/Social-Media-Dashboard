ea7f6c26ac2bab2283cdd0b754e6f192e02ba481
bryaaaamador <bryaaaamador@gmail.com>
2022-04-25 23:33:50 -0500

    actualizado

--- src/top-card-list.js
+++ src/top-card-list.js
@@ -45,49 +45,6 @@ function TopCardList() {
           {
             cardListData.map((cardData) => <Card key={cardData.id} {...cardData}/>)
           }
-
-          {/*<article className="card twitter">
-            <p className="card-title">
-              <img src="images/icon-twitter.svg" alt="" />
-              @LeonidasEsteban
-            </p>
-            <p className="card-followers">
-              <span className="card-followers-number">28k</span>
-              <span className="card-followers-title">Followers</span>
-            </p>
-            <p className="card-today">
-              <img src="images/icon-up.svg" alt="" />
-              12 Today
-            </p>
-          </article>
-          <article className="card instagram">
-            <p className="card-title">
-              <img src="images/icon-instagram.svg" alt="" />
-              @LeonidasEsteban
-            </p>
-            <p className="card-followers">
-              <span className="card-followers-number">6k</span>
-              <span className="card-followers-title">Followers</span>
-            </p>
-            <p className="card-today">
-              <img src="images/icon-up.svg" alt="" />
-              12 Today
-            </p>
-          </article>
-          <article className="card youtube">
-            <p className="card-title">
-              <img src="images/icon-youtube.svg" alt="" />
-              @LeonidasEsteban
-            </p>
-            <p className="card-followers">
-              <span className="card-followers-number">12k</span>
-              <span className="card-followers-title">Followers</span>
-            </p>
-            <p className="card-today">
-              <img src="images/icon-up.svg" alt="" />
-              12 Today
-            </p>
-  </article>*/}
         </div>
       </div>
     </section>
